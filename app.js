985954b1f687383803bcf78c18f3bc37a9cf8b69
AviramV <aviramv2@gmail.com>
2023-02-19 16:04:52 +0200

    add connecting app and database

--- app.js
+++ app.js
@@ -3,8 +3,21 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 4001;
 
+const db = require('./db');
+
 app.get('/', (req, res) => {
     res.send('Hello World');
+})
+
+app.get('/test', async(req, res) => {
+    const dbRes = await db.query(`SELECT table_name 
+    FROM information_schema.tables 
+    WHERE table_schema = 'public' 
+      AND table_type = 'BASE TABLE';`)
+    res.send(dbRes.rows);
+    dbRes.rows.forEach(row => {
+        console.log(row)
+    })
 })
 
 app.listen(PORT, () => {
