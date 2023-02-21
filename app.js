7b9c1e5fa6e0ff5411bcf62b073d5da4eb72dbad
AviramV <aviramv2@gmail.com>
2023-02-21 17:14:16 +0200

    implement local strategy

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ require ('dotenv').config();
 const express = require('express');
 const app = express();
 const session = require('express-session');
+const passport = require('passport');
+const localStrategy = require('passport-local');
 const db = require('./db');
 
 const PORT = process.env.PORT || 4001;
@@ -15,6 +17,20 @@ app.use(
         saveUninitialized: false
     })
 );
+
+app.use(passport.initialize(), passport.session());
+
+// Add passport local strategy
+passport.use(
+    new localStrategy(function (username, password, done) {
+      db.users.findByUsername(username, (err, user) => {
+        if (err) return done(err);
+        if (!user) return done(null, false);
+        if (user.password != password) return done(null, false);
+        return done(null, user);
+      });
+    })
+  );
 
 app.get('/', (req, res) => {
     res.send('Hello World');
