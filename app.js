91e9ac468ce92e038c36a5e345efbdbba6b05ed5
AviramV <aviramv2@gmail.com>
2023-02-24 15:50:53 +0200

    Add logic for handling login & logout

--- app.js
+++ app.js
@@ -4,12 +4,15 @@ const app = express();
 const session = require('express-session');
 const passport = require('passport');
 const bcrypt = require('bcrypt');
-const localStrategy = require('passport-local').Strategy;
+const localStrategy = require('passport-local');
 const db = require('./db');
 
 const PORT = process.env.PORT || 4001;
 
 const secret = process.env.SECRET;
+
+// ****** MIDDLEWARE ****** //
+
 app.use(
     session({
         secret,
@@ -19,47 +22,43 @@ app.use(
     })
 );
 
-const passwordHash = async (password, saltRounds) => {
-    try {
-      const salt = await bcrypt.genSalt(saltRounds);
-      const hash = await bcrypt.hash(password, salt);
-      return hash;
-    } catch (err) {
-      console.log(err);
-    }
-    return null;
-  };
-
 app.use(passport.initialize(), passport.session());
 
-passport.serializeUser((user, done) => {
-    done(null, user.id);
-  });
-  
-  passport.deserializeUser((id, done) => {
-    db.users.findById(id, function (err, user) {
-      if (err) {
-        return done(err);
-      }
-      done(null, user);
-    });
-  });
-
-// Add passport local strategy
+// Configure passport local strategy
 passport.use(
-    new localStrategy(function (username, password, done) {
-        db.users.findByUsername(username, (err, user) => {
-            if (err) return done(err);
-            if (!user) return done(null, false);
-            if (user.password != password) return done(null, false);
+    new localStrategy(async function (username, password, done) {
+        const failMessage = { message: 'Incorrect username or password.' }
+        try {
+            const user = await getUser(username);
+            if (!user) return done(null, false, failMessage);
+
+            const matchedPassword = await bcrypt.compare(password, user.password);
+            if (!matchedPassword) return done(null, false, failMessage);
+
             return done(null, user);
-        });
+        } catch (error) {
+            return done(error);
+        }
+        
     })
 );
 
+passport.serializeUser((user, done) => {
+      done(null, {
+        id: user.id,
+        username: user.user_name
+    });
+});
+
+passport.deserializeUser((user, done) => {
+     done(null, user)
+})
+
+// ****** ROUTES ****** //
+
 app.get('/', (req, res) => {
     res.send('Hello World');
-})
+});
 
 app.post('/register', express.json(), async (req, res) => {
     const { email, username, password } = req.body;
@@ -71,8 +70,46 @@ app.post('/register', express.json(), async (req, res) => {
     } catch (error) {
         res.status(409).send(error.detail)
     }
+});
+
+app.get('/login', (req, res) => {
+    if (!req.user) return res.send('not logged in')
+    res.send(`Hello ${req.user.username}`)
+})
+
+app.post('/login', express.json(), passport.authenticate('local', {failureMessage: true}), (req, res) => {
+    res.send(`Logged in as ${req.body.username}`);
+})
+
+app.post('/logout', (req, res) => {
+    const isLoggedIn = req.user;
+    if (!isLoggedIn) return res.send(`Nothing to do here`);
+    req.logout(err => {
+        if (err) return res.send(err)
+        res.send(`Logged out successfully`)
+    });
 })
 
 app.listen(PORT, () => {
     console.info(`Server running on port ${PORT}`);
-})
+});
+
+// ****** UTILS ******* //
+
+// Hash and salt a password
+async function passwordHash(password, saltRounds) {
+    try {
+        const salt = await bcrypt.genSalt(saltRounds);
+        const hash = await bcrypt.hash(password, salt);
+        return hash;
+    } catch (err) {
+        console.log(err);
+    }
+    return null;
+};
+
+// Get user from database by username
+async function getUser(username) {
+    const user = await db.query(`SELECT * FROM users WHERE user_name = $1`, [username]);
+    return user.rows[0];
+}
